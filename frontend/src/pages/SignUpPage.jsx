add202e601a2044f62dd509f75fb4eb887f03f80
5ujit <sksujit021@gmail.com>
2025-05-18 23:57:24 +0530

    frontend

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAuthStore } from '../store/use.AuthStore';
+
 
 const SignUpPage = () => {
+  const [shoPassword,setPassword]=useState(false);
+  const [formData,setFormdata]=useState({
+    fullName:"",
+    email: "",
+    password: "",
+  });
+  const {signup ,isSigningUp}= useAuthStore();
   return (
     <div>
       SignUpPage....
