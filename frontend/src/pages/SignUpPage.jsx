577d79a97b36ff87fd0d875b2f67657f2cf0ef73
5ujit <sksujit021@gmail.com>
2025-06-09 19:38:48 +0530

    Update SignUpPage.jsx

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,24 +1,48 @@
-import React, { useState } from 'react'
-import { useAuthStore } from '../store/use.AuthStore';
-
-
+import React, { useState } from "react";
+import { useAuthStore } from "../store/use.AuthStore";
+import { MessageSquare } from "lucide-react";
 const SignUpPage = () => {
-  const [shoPassword,setPassword]=useState(false);
-  const [formData,setFormdata]=useState({
-    fullName:"",
+  const [shoPassword, setPassword] = useState(false);
+  const [formData, setFormdata] = useState({
+    fullName: "",
     email: "",
     password: "",
   });
-  const {signup ,isSigningUp}= useAuthStore();
-  const validateForm= ()=>{}
-  const handleSubmit= (e)=>{
-    e.preventDefault()
-  }
+  const { signup, isSigningUp } = useAuthStore();
+  const validateForm = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      SignUpPage....
-    </div>
-  )
-}
+    <div className="min-h-screen grid lg:grid-cols-2">
+      {/* left side  */}
+      <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+        <div className="w-full max-w-md space-y-8">
+          {/* LOGO */}
+          <div className="text-center mb-8">
+            <div className="flex flex-col items-center gap-2 group">
+              <div
+                className="size-12 rounded-xl bg-primary/10 flex items-center justify-center
+              group-hover:bg-primary/20 translate-colors"
+              >
+                <MessageSquare className="size-6 text-primary" />
+              </div>
+              <h1 className="text-2xl font-bold mt-2">Create Account </h1>
+              <p className="text-base-content/60">
+                Get started with your free account
+              </p>
+            </div>
+          </div>
 
-export default SignUpPage
+          <form onSubmit={handleSubmit} className="space-y-6">     
+
+          </form>
+        
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
+// 1:54
