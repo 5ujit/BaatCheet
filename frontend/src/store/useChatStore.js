68765908053b76edea20da570df9ea2b887f531c
5ujit <sksujit021@gmail.com>
2025-07-20 23:28:42 +0530

    Update useChatStore.js

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,13 +1,37 @@
-import{create} from "zustand"
+import { create } from "zustand"
 import toast from "react-hot-toast";
-import {axiosInstance} from "../lib/axios"
+import { axiosInstance } from "../lib/axios"
 
-export const useChatStore= create((set)=>({
-message:[],
-users: [],
-selectUser: null,
-isUserLoading: false,.
-isMessage``
+export const useChatStore = create((set) => ({
+    message: [],
+    users: [],
+    selectUser: null,
+    isUserLoading: false,
+    isMessagesLoading: false,
 
 
-}))``
+    getUsers: async () => {
+        set({ isUsersLoading: true });
+        try {
+            const res = await axiosInstance.get("/message/users");
+            set({ users: res.data });
+        } catch (error) {
+            toast.error(error.response.data.message)
+        } finally {
+            set({ isUserLoading: false });
+        }
+    },
+    getmessages: async () => {
+        set({ sisMessagesLoading: true })
+        try {
+            const res = await axiosInstance.get(`/messages/${userId}`);
+            set({ messages: res.data })
+
+        } catch (error) {
+                toast.error(error.response.data.message);
+        } finally {
+            set({ isMessagesLoading: false });
+        }
+
+    }
+}))
