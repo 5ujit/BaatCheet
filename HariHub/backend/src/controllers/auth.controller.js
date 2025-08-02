86207dd282cf3cca6e9407e5bbaed47c03a7fa28
5ujit <sksujit021@gmail.com>
2025-08-02 23:39:29 +0530

    Update auth.controller.js

--- HariHub/backend/src/controllers/auth.controller.js
+++ HariHub/backend/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 // backend > src> controllers> auth.controller.js
-import User from "../models/user.model";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs"
 export const signup=async (req,res)=>
     {
         const {fullName,email, password}= req.body 
@@ -8,8 +9,17 @@ export const signup=async (req,res)=>
         if(password.length <6){
             return res.status(400).json({message:"Password must be at least 6 characters "});
         }
-        const user= await user.findOne({email})
+        const user= await User.findOne({email})
         if (user) res.status(400).json({message:"Email alreaddy exist"})
+            const salt = await bcrypt.genSalt(10)
+            const hashedPassword = await  bcrypt.hash(password,salt)
+           
+           
+            const newUser=new User({
+                fullNamme:fullName,
+                email:email,
+                password:hashedPassword
+            })
     } catch (error) {
         
     }
